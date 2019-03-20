30c9158627975b1b7c7606bcdacc2814128c2964
Alex <alex.cicioc1@gmail.com>
2019-03-20 08:30:07 +0200

    Cursul 2 - CSS basics & animations, js basics

---
+++ js/game.js
@@ -0,0 +1,32 @@
+const string = 'numele meu este ';
+console.log(string);
+
+// let numeleMeu = 'alex';
+let number = 2;
+let number2 = 5;
+// console.log(string, number);
+// console.log(number * number2);
+// console.log(string + numeleMeu);
+
+// console.log(number / number2);
+// console.log(number2 % number == 0);
+// console.log(`${numeleMeu} ${number}`);
+// console.log(Math.pow(number, number2))
+
+// string = 'numele meu a fost';
+// console.log(string);
+
+// const PI = 3.14;
+
+let conditie = true;
+conditie = false;
+
+conditie = number2 % number == 0;
+console.log(conditie);
+
+if (conditie === true) {
+  console.log('numarul e par');
+} else {
+  console.log('numarul e impar');
+}
+
